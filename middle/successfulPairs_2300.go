dedeb783eb3c42d38c0ee7d512324fa660199c5f
dengcl <1985144542@qq.com>
2023-11-10 14:03:16 +0800

    daily exercise

--- middle/successfulPairs_2300.go
+++ middle/successfulPairs_2300.go
@@ -21,5 +21,22 @@ func successfulPairs(spells []int, potions []int, success int64) []int {
 }
 
 func successfulPairs_double_pointer(spells []int, potions []int, success int64) []int {
-
+	sort.Ints(potions)
+	tmp := [][2]int{}
+	for i, v := range spells {
+		tmp = append(tmp, [2]int{i, v})
+	}
+	sort.Slice(tmp, func(i, j int) bool {
+		return tmp[i][1] > tmp[j][1]
+	})
+	spellIdx, potionIdx := 0, 0
+	res := make([]int, len(spells))
+	for spellIdx < len(spells) {
+		for potionIdx < len(potions) && tmp[spellIdx][1]*potions[potionIdx] < int(success) {
+			potionIdx++
+		}
+		res[tmp[spellIdx][0]] = len(potions) - potionIdx
+		spellIdx++
+	}
+	return res
 }
